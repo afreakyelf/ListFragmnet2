c52d3f6dae6fe321040ccb785622a431a943a615
afreakyelf <rajat.mittal.15cse@bml.edu.in>
2018-06-29 10:24:02 +0530

    With onclickitemlistener

--- app/src/main/java/com/example/rajat/fragmentexample/FragA.java
+++ app/src/main/java/com/example/rajat/fragmentexample/FragA.java
@@ -10,8 +10,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class FragA extends ListFragment {
 
@@ -23,9 +25,21 @@ public class FragA extends ListFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_frag,null);
 
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),R.array.mydata,android.R.layout.simple_list_item_1);
+        final ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),R.array.mydata,android.R.layout.simple_list_item_1);
         setListAdapter(adapter);
 
+
         return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
