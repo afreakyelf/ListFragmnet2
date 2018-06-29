238daf37ab9ead805e31328fd082d66e5dab0c8d
afreakyelf <rajat.mittal.15cse@bml.edu.in>
2018-06-29 10:06:29 +0530

    Second

---
+++ app/src/main/java/com/example/rajat/fragmentexample/FragA.java
@@ -0,0 +1,31 @@
+package com.example.rajat.fragmentexample;
+
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class FragA extends ListFragment {
+
+    ListView listView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_frag,null);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),R.array.mydata,android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/rajat/fragmentexample/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.rajat.fragmentexample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.content,new FragA(),"a");
+        ft.commit();
+
+    }
+}
